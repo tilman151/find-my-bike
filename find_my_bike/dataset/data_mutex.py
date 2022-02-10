30096bebbbedf915ac9045661011f0d0cbdd8a4a
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-02-10 13:46:39 +0100

    Add mutual exclusivity checking script.

---
+++ find_my_bike/dataset/data_mutex.py
@@ -0,0 +1,53 @@
+import itertools
+import os
+import json
+from functools import lru_cache
+from typing import Any
+
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+
+
+def data_mutex(path_a: str, path_b: str) -> None:
+    meta_a = load_meta(path_a)
+    meta_b = load_meta(path_b)
+    product = itertools.product(meta_a.keys(), meta_b.keys())
+    duplicates = []
+    for img_file_a, img_file_b in tqdm(product, total=len(meta_a) * len(meta_b)):
+        img_a = _load_image(path_a, img_file_a)
+        img_b = _load_image(path_b, img_file_b)
+        if not img_a.shape == img_b.shape:
+            continue
+        elif np.sum(img_a - img_b) == 0:
+            duplicates.append(f"{path_a}: {img_file_a} is {path_b}: {img_file_b}")
+
+    print(*duplicates, sep="\n")
+
+
+@lru_cache(maxsize=300)
+def _load_image(path, img_file_name):
+    img = Image.open(os.path.join(path, img_file_name))
+    img = np.array(img)
+
+    return img
+
+
+def load_meta(data_path: str) -> Any:
+    with open(os.path.join(data_path, "meta.json"), mode="rt") as f:
+        data = json.load(f)
+
+    return data
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Check two datasets for mutual exclusivity."
+    )
+    parser.add_argument("path_a", help="Path to first dataset")
+    parser.add_argument("path_b", help="Path to second dataset")
+    opt = parser.parse_args()
+
+    data_mutex(opt.path_a, opt.path_b)
