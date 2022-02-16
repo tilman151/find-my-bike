ccceb07b6580813b60f0e3d9a418df401e9abd6a
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-02-16 14:54:33 +0100

    Fill in all annotated aspects from Label Studio when converting to meta.json.

--- find_my_bike/dataset/ls2meta.py
+++ find_my_bike/dataset/ls2meta.py
@@ -1,6 +1,16 @@
-import os
 import json
-from typing import Dict, Any, List
+import os
+from typing import Dict, Any, List, Tuple
+
+_LS_INFO_KEYS = {
+    "image",
+    "id",
+    "annotator",
+    "annotation_id",
+    "created_at",
+    "updated_at",
+    "lead_time",
+}
 
 
 def labelstudio2meta(
@@ -8,17 +18,31 @@ def labelstudio2meta(
 ) -> Dict[str, Dict[str, Any]]:
     for anno_info in annotations:
         image_file = os.path.basename(anno_info["image"])
-        new_label = anno_info["bike"].lower().replace(" ", "_")
-        image_labels = meta[image_file]["labels"]
-        old_label = image_labels["bike"] if "bike" in image_labels else ""
-        if not new_label == old_label:
-            print(f"Image {image_file} differs: Meta({old_label}) vs. LS({new_label})")
-        image_labels["bike"] = new_label
+        for aspect in set(anno_info.keys()).difference(_LS_INFO_KEYS):
+            new_label = _norm_label(anno_info[aspect])
+            _set_aspect_label(aspect, new_label, image_file, meta)
 
     return meta
 
 
-def load_json(data_path: str) -> Any:
+def _norm_label(label: str) -> str:
+    return label.lower().replace(" ", "_")
+
+
+def _set_aspect_label(
+    aspect: str, new_label: str, image_file: str, meta: Dict[str, Dict[str, Any]]
+) -> None:
+    image_labels = meta[image_file]["labels"]
+    old_label = image_labels[aspect]
+    if not new_label == old_label:
+        print(
+            f"Image {image_file} differs in '{aspect}':"
+            f"Meta({old_label}) vs. LS({new_label})"
+        )
+        image_labels[aspect] = new_label
+
+
+def load_json(data_path: str) -> Tuple[Any, Any]:
     with open(os.path.join(data_path, "annotations.json"), mode="rt") as f:
         anno = json.load(f)
     with open(os.path.join(data_path, "meta.json"), mode="rt") as f:
@@ -42,7 +66,7 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(
-        description="Convert meta JSON to Label Studio format."
+        description="Convert Label Studio annotations to meta JSON."
     )
     parser.add_argument("data_path", help="Path to dataset")
     opt = parser.parse_args()
