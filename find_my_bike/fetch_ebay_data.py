714fc41e5a2c6fb7ef50252d6ee93c53d77681b4
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-02-22 20:59:04 +0100

    Add some more logging to download script.

--- find_my_bike/fetch_ebay_data.py
+++ find_my_bike/fetch_ebay_data.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import hydra
@@ -5,6 +6,8 @@ from omegaconf import DictConfig
 
 from find_my_bike.dataset import download_images
 from find_my_bike.scrape import EbayImageScraper
+
+logger = logging.getLogger(__name__)
 
 DATA_ROOT = os.path.join(os.path.dirname(__file__), "..", "data")
 
@@ -15,8 +18,11 @@ def fetch_ebay_val_data(config: DictConfig) -> None:
     with EbayImageScraper() as scraper:
         image_urls = scraper.get_items(config.query, config.location, num_images)
     dataset_folder = os.path.join(DATA_ROOT, config.dataset_name)
+
+    logger.info("Download training images")
     train_images = image_urls[: config.num_train_images]
     download_images(train_images, dataset_folder + "_train", config.aspects)
+    logger.info("Download validation images")
     val_images = image_urls[config.num_train_images :]
     download_images(val_images, dataset_folder + "_val", config.aspects)
 
