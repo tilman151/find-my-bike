1fe80665efd8d3aa2b5e166187d2c50b085d8e5b
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-02-24 09:46:58 +0100

    Add function to retrieve high res versions of images from ebay.

--- find_my_bike/dataset/utils.py
+++ find_my_bike/dataset/utils.py
@@ -69,6 +69,15 @@ def _write_meta(meta: Dict[str, Any], output_folder: str) -> None:
         logger.debug("Skip writing empty meta.json")
 
 
+def download_high_res_images(meta: Dict[str, Dict[str, Any]], output_folder: str):
+    for image_file, image_info in meta.items():
+        high_res_url = image_info["image_url"].replace("$_2.JPG", "$_59.JPG")
+        high_res_file = image_file.replace(".jpg", "_highres.jpg")
+        high_res_path = os.path.join(output_folder, high_res_file)
+        response = requests.get(high_res_url, stream=True)
+        _write_image(response, high_res_path)
+
+
 def load_annotations(data_path: str) -> List[Dict[str, Any]]:
     with open(os.path.join(data_path, "annotations.json"), mode="rt") as f:
         anno = json.load(f)
